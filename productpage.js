2c19a93ede264951faadc7bb31ca622874e77d43
Lasse Joergensen <lass876q@stud.kea.dk>
2021-11-12 18:14:55 +0100

    greatstrideshavebeendone missing finishing touch

---
+++ productpage.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", textcarusel);
+
+function textcarusel() {
+  document.querySelector(".rotatingText-adjective1").classList.add("rot1");
+  document.querySelector(".rotatingText-adjective2").classList.add("rot2");
+  document.querySelector(".rotatingText-adjective3").classList.add("rot3");
+  document.querySelector(".rotatingText-adjective4").classList.add("rot4");
+}
+
+/* --------------- COLLAPSED SIDE BAR ------------------- */
+
+document.querySelector("#open").addEventListener("click", openSideMenu);
+let flag;
+
+function openSideMenu() {
+  document.querySelector(".side-nav").classList.toggle("width");
+  var tl = gsap.timeline();
+
+  if (flag) {
+    flag = 0;
+    /* tl.reverse(); */
+    tl.to("rect.one", { opacity: 1, duration: 0.35, rotation: 0 });
+    tl.to("rect.two", { opacity: 1, duration: 0.7 }, "<");
+    tl.to("rect.three", { opacity: 1, duration: 0.35, rotation: 0 }, "<");
+  } else {
+    flag = 1;
+    tl.to("rect.one", { duration: 0.35, rotation: 42 });
+    tl.to("rect.two", { opacity: 0, duration: 0.7 }, "<");
+    tl.to("rect.three", { duration: 0.35, rotation: -42 }, "<");
+  }
+}
